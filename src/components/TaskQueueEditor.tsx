3bd9c8d862f63654f15ff0b957e549a4623fcce3
Ash <ext0l@catgirl.ai>
2022-01-16 18:41:56 -0800

    Mark resource requirements

--- src/components/TaskQueueEditor.tsx
+++ src/components/TaskQueueEditor.tsx
@@ -43,11 +43,24 @@ const TaskQueueEditor = observer((props: Props) => {
   const addButtons = Object.values(TASKS)
     .filter((task) => task.visible(player))
     .map((task) => {
+      const requirements = Object.entries(task.requiredStats).map(
+        ([name, min]) => (
+          <span>
+            {name} {min}
+          </span>
+        )
+      );
       const tooltip = (
         <div className="w-96 p-2">
           <p>
             <strong>Cost:</strong> {task.baseCost}
           </p>
+          {Object.keys(task.requiredStats).length !== 0 && (
+            <p>
+              <strong>Requires: </strong>
+              {requirements}
+            </p>
+          )}
           <hr className="border-gray-700 my-3" />
           <p className="text-sm mb-2 text-gray-400">{task.description}</p>
         </div>
