ca9b9aa1a404944375958504612f87f5bd3bdcea
Ash <ext0l@catgirl.ai>
2022-08-18 15:38:21 -0700

    Memoize TaskIcon.

--- src/components/common/TaskIcon.tsx
+++ src/components/common/TaskIcon.tsx
@@ -9,6 +9,7 @@ import {
   GiTargeting,
 } from "react-icons/gi";
 import { FaVenusDouble } from "react-icons/fa";
+import React from "react";
 
 // because it's funny to me
 const DEBUG_ICON = FaVenusDouble;
@@ -29,8 +30,9 @@ type Props = {
   className?: string;
 };
 
-export const TaskIcon = (props: Props) => {
+export const TaskIcon = React.memo((props: Props) => {
   const { task, size, className } = props;
   const Icon = ICONS[task]!;
   return <Icon className={className} size={size || "1.5em"} />;
-};
+});
+TaskIcon.displayName = "TaskIcon";
