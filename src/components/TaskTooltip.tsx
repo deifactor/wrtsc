d5ccd309472a8312e32b3bb2df8d1cf7561d7a51
Ash <ext0l@catgirl.ai>
2022-08-18 21:59:47 -0700

    Display rewards in tooltip as well.

--- src/components/TaskTooltip.tsx
+++ src/components/TaskTooltip.tsx
@@ -9,6 +9,7 @@ import {
 } from "../engine";
 import React from "react";
 import { useEngineSelector } from "../engineStore";
+import { entries } from "../records";
 
 interface Props {
   kind: TaskKind;
@@ -23,24 +24,46 @@ export const TaskTooltip = React.memo(({ kind }: Props) => {
       {task.trainedSkills.map((s) => SKILL_NAME[s]).join(", ")}
     </p>
   );
-  const progressFrags = Object.entries(task.required.progress || {}).map(
-    ([progress, min]) => `${min}% ${PROGRESS_NAME[progress as ProgressId]}`
-  );
-  const resourceFrags = Object.entries(task.required.resources || {}).map(
-    ([resource, amount]) =>
-      `${amount}x ${RESOURCES[resource as ResourceId].name}`
-  );
-  const frags = [...progressFrags, ...resourceFrags];
-  const requiresSection = frags.length !== 0 && (
+
+  const requiresFrags = [
+    ...Object.entries(task.required.progress || {}).map(
+      ([progress, min]) => `${min}% ${PROGRESS_NAME[progress as ProgressId]}`
+    ),
+    ...Object.entries(task.required.resources || {}).map(
+      ([resource, amount]) =>
+        `${amount}x ${RESOURCES[resource as ResourceId].name}`
+    ),
+  ];
+  const requiresSection = requiresFrags.length !== 0 && (
     <div>
       <strong>Requires</strong>:
       <ul>
-        {frags.map((frag) => (
-          <li key={frag}>{frag}</li>
+        {requiresFrags.map((frag, index) => (
+          <li key={index}>{frag}</li>
         ))}
       </ul>
     </div>
   );
+
+  const rewardsFrags = [
+    ...entries(task.rewards.progress || {}).map(
+      ([progress]) => `${PROGRESS_NAME[progress]} progress`
+    ),
+    ...entries(task.rewards.resources || {}).map(
+      ([resource, amount]) => `${amount}x ${RESOURCES[resource].name}`
+    ),
+  ];
+  const rewardsSection = rewardsFrags.length !== 0 && (
+    <div>
+      <strong>Rewards</strong>:
+      <ul>
+        {rewardsFrags.map((frag, index) => (
+          <li key={index}>{frag}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
     <div className="w-96 p-2 text-sm">
       <p className="font-bold">{task.name}</p>
@@ -49,6 +72,7 @@ export const TaskTooltip = React.memo(({ kind }: Props) => {
         <strong>Cost:</strong> {cost}
       </p>
       {requiresSection}
+      {rewardsSection}
       {trainingSection}
       <hr className="border-gray-700 my-3" />
       <p className="text-xs mb-2 text-gray-400">{task.flavor}</p>
