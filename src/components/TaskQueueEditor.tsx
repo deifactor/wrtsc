fde2a809d29e6fa96f68f66cee0dab532fbbf499
Ash <ext0l@catgirl.ai>
2022-08-25 22:34:42 -0700

    Split out TaskQueueEditor's item into its own element.
    This lets us use useCallback etc.

--- src/components/TaskQueueEditor.tsx
+++ src/components/TaskQueueEditor.tsx
@@ -17,72 +17,73 @@ import {
 import equal from "fast-deep-equal";
 import { useAppDispatch, useAppSelector, useEngineSelector } from "../store";
 
-interface Props {
-  className?: string;
-}
-
-const TaskQueueEditor = React.memo((props: Props) => {
+const TaskQueueItem = React.memo(({ index }: { index: number }) => {
   const dispatch = useAppDispatch();
-  const queue = useAppSelector((store) => store.engine.nextQueue);
-  const simulation = useAppSelector((store) => store.engine.simulation, equal);
-  const { className } = props;
-  const tasks = queue.map((entry, index) => {
-    const incrementCount = () =>
-      dispatch(modifyBatchCount({ index, amount: 1 }));
-    const decrementCount = () =>
-      dispatch(modifyBatchCount({ index, amount: -1 }));
-    const moveUp = () => dispatch(moveTask({ from: index, to: index - 1 }));
-    const moveDown = () => dispatch(moveTask({ from: index, to: index + 1 }));
-    const remove = () => dispatch(removeTask(index));
-    const maxClass = classNames({
-      invisible: !TASKS[entry.task].maxIterations,
-    });
-    // We always show the button for consistent sizing.
-    const setToMaxButton = (
-      <Button
-        className={maxClass}
-        size="xs"
-        onClick={() => dispatch(setBatchCountToMax(index))}
-      >
-        <FiMaximize />
-      </Button>
-    );
-    const step: SimulationStep | undefined = simulation[index];
-    return (
-      // eslint-disable-next-line react/no-array-index-key
-      <div
-        key={index}
-        className={classNames("flex items-center h-10", {
-          "text-red-300": !step?.ok,
-        })}
-      >
-        <div className="flex-grow font-mono text-lg">
-          <TaskIcon className="inline" task={entry.task} /> x{entry.count}
-        </div>
-        <div className="px-4 text-yellow-300 font-mono font-bold text-lg">
-          {step?.energy}
-        </div>
-        <div className="text-sm">
-          {setToMaxButton}
-          <Button size="xs" onClick={incrementCount}>
-            <FaPlus size="1em" />
-          </Button>
-          <Button size="xs" onClick={decrementCount}>
-            <FaMinus />
-          </Button>
-          <Button size="xs" onClick={moveUp}>
-            <FaArrowUp />
-          </Button>
-          <Button size="xs" onClick={moveDown}>
-            <FaArrowDown />
-          </Button>
-          <Button size="xs" onClick={remove}>
-            <RiDeleteBackFill />
-          </Button>
-        </div>
-      </div>
-    );
+  const entry = useAppSelector((store) => store.engine.nextQueue[index], equal);
+  const step: SimulationStep | undefined = useAppSelector(
+    (store) => store.engine.simulation[index],
+    equal
+  );
+  const incrementCount = () => dispatch(modifyBatchCount({ index, amount: 1 }));
+  const decrementCount = () =>
+    dispatch(modifyBatchCount({ index, amount: -1 }));
+  const moveUp = () => dispatch(moveTask({ from: index, to: index - 1 }));
+  const moveDown = () => dispatch(moveTask({ from: index, to: index + 1 }));
+  const remove = () => dispatch(removeTask(index));
+  const maxClass = classNames({
+    invisible: !TASKS[entry.task].maxIterations,
   });
+  // We always show the button for consistent sizing.
+  const setToMaxButton = (
+    <Button
+      className={maxClass}
+      size="xs"
+      onClick={() => dispatch(setBatchCountToMax(index))}
+    >
+      <FiMaximize />
+    </Button>
+  );
+  return (
+    // eslint-disable-next-line react/no-array-index-key
+    <div
+      key={index}
+      className={classNames("flex items-center h-10", {
+        "text-red-300": !step?.ok,
+      })}
+    >
+      <div className="flex-grow font-mono text-lg">
+        <TaskIcon className="inline" task={entry.task} /> x{entry.count}
+      </div>
+      <div className="px-4 text-yellow-300 font-mono font-bold text-lg">
+        {step?.energy}
+      </div>
+      <div className="text-sm">
+        {setToMaxButton}
+        <Button size="xs" onClick={incrementCount}>
+          <FaPlus size="1em" />
+        </Button>
+        <Button size="xs" onClick={decrementCount}>
+          <FaMinus />
+        </Button>
+        <Button size="xs" onClick={moveUp}>
+          <FaArrowUp />
+        </Button>
+        <Button size="xs" onClick={moveDown}>
+          <FaArrowDown />
+        </Button>
+        <Button size="xs" onClick={remove}>
+          <RiDeleteBackFill />
+        </Button>
+      </div>
+    </div>
+  );
+});
+
+const TaskQueueEditor = React.memo((props: { className?: string }) => {
+  const dispatch = useAppDispatch();
+  const { className } = props;
+  const length = useAppSelector((store) => store.engine.nextQueue.length);
+  const indices = Array.from(Array(length).keys());
 
   const addButtons = useEngineSelector((engine) => Object.values(engine.tasks))
     .filter((task) => task.visible)
@@ -104,7 +105,11 @@ const TaskQueueEditor = React.memo((props: Props) => {
   // vertical alignment.
   return (
     <div className={classNames("flex flex-col", className)}>
-      <div className="flex-auto overflow-y-scroll">{tasks}</div>
+      <div className="flex-auto overflow-y-scroll">
+        {indices.map((index) => (
+          <TaskQueueItem key={index} index={index} />
+        ))}
+      </div>
       <div>{addButtons}</div>
     </div>
   );
