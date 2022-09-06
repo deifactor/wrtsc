d00bb032eac5eb24164b4695e77011fc20ee7054
Ash <ext0l@catgirl.ai>
2022-09-06 16:17:19 -0700

    chore: set up rules for scope/type enums

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,24 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        "ui",
+        // for CSS-ish things
+        "styling",
+        // documentation
+        "docs",
+        // generic engine tasks
+        "engine",
+        "simulant",
+        "skill",
+        "task",
+        // balance tasks
+        "balance",
+      ],
+    ],
+    "type-enum": [2, "always", ["fix", "feat", "test", "chore", "refactor"]],
+  },
+};
